8c2c37af977a8a5ad88ac99be3867095ab8b9ddd
Bishalsarang <bishalsarang@gmail.com>
2024-05-10 22:59:29 +0545

    Fix image component

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,7 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ src, ...rest }: ImageProps) => (
+  <NextImage src={`/bishalsarang.github.io${src}`} {...rest} />
+)
 
 export default Image
